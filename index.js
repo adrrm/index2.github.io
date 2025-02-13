b1f2a77c27aa1796cec4df8ff31dbedc7e293511
adrrm <alexandra29j@gmail.com>
2025-02-12 20:04:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function sumar(valor1 , valor2){
+    var resultado= valor1 + valor2;
+    return resultado;
+}
+    document.getElementById('resultado').innerHTML="Resultado de la suma es:"+sumar(50,20);
+
+function restar(valor3 , valor4){
+    var resultado= valor3 - valor4;
+    return resultado;
+}
+    document.getElementById('resultado').innerHTML="Resultado de la resta es:"+restar(10,5);
+
+
+function multiplicar(valor5 , valor6){
+    var resultado= valor5 * valor6;
+    return resultado;
+}
+    document.getElementById('resultado').innerHTML="Resultado de la multiplicacion es:"+multiplicar(60,4);
+
+function dividir(valor7 , valor8){
+    var resultado= valor8/valor7;
+    return resultado;
+}
+    document.getElementById('resultado').innerHTML="Resultado de la division es:"+dividir(80,40);
