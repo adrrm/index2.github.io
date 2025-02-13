c4f471b0cb4afd7a418e4155c10ebecb13666357
adrrm <alexandra29j@gmail.com>
2025-02-12 20:12:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function sumar(valor1 , valor2){
-    var resultado= valor1 + valor2;
+    var resultado= valor1+valor2;
     return resultado;
 }
     document.getElementById('resultado1').innerHTML="Resultado de la suma es:"+sumar(50,20);
 
 function restar(valor3 , valor4){
-    var resultado= valor3 - valor4;
+    var resultado= valor3-valor4;
     return resultado;
 }
     document.getElementById('resultado2').innerHTML="Resultado de la resta es:"+restar(10,5);
 
 
 function multiplicar(valor5 , valor6){
-    var resultado= valor5 * valor6;
+    var resultado= valor5*valor6;
     return resultado;
 }
     document.getElementById('resultado3').innerHTML="Resultado de la multiplicacion es:"+multiplicar(60,4);
